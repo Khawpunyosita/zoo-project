2c3818aad792ace0edeccda0cf6180689e8c1337
Yosita Khundilorkkhunakron <yosita.khun@kmutt.ac.th>
2024-11-27 09:19:27 +0000

    unit test case

--- test_zoo.py
+++ test_zoo.py
@@ -5,9 +5,24 @@ class TestZoo(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
 
-    def test_child_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(5), 50)
-       
+    def test_negative_age_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), 0)
+
+    def test_0_to_12_age_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+
+    def test_13_to_20_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+
+    def test_21_to_60_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)
+    
+    def test_more_than_60_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
+    
     # Add your additional test cases here.
 
 if __name__ == '__main__':
